0539b0bebf3d53d84cc34f534bcf02482db59f8c
Gabriel <gabesciechan@yahoo.co.uk>
2020-04-01 11:34:59 +0100

    setup component file structure

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./TrackList.css";
+import Track from "../Track/Track";
+
+class TrackList extends React.Component {
+  render() {
+    return (
+      <div className="TrackList">
+        <Track />
+        <Track />
+        <Track />
+      </div>
+    );
+  }
+}
+
+export default TrackList;
